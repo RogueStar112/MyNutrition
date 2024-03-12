fca8ca01b0cf43b390c77d5356958505fa889efb
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-03-12 20:12:27 +0000

    12032024

--- resources/views/exercise_form.blade.php
+++ resources/views/exercise_form.blade.php
@@ -1,15 +1,64 @@
 <x-app-layout>
   <x-slot name="header">
-      <h2 class="font-semibold  text-center italic uppercase dark:text-white text-3xl text-gray-800 leading-tight">
+      <h1 class="font-semibold  text-center italic uppercase dark:text-white text-3xl text-gray-800 leading-tight">
           {{ __('Exercise Main Menu') }}
-      </h2>
+      </h1>
   </x-slot>
 
   <div class="py-4">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
         
+
+        
+
+        <div class="max-w-3xl px-24 border-4 text-white border-yellow-400 bg-gray-800 [&>*]:mt-4 h-[512px]">
+
+
+            
+            <div class="w-full">
+                <label for="exercise-time" class="text-white"><p class="font-bold text-xl text-center">Exercise time</p>
+
+                    <input type="datetime-local" name="exercise-time" class="block bg-slate-700 text-gray-200 w-full mt-1 rounded-md" placeholder="Big Breakfast, Spaghetti Carbonara, BLT Sandwich" value="" required/>
+                </label>
+            </div>
+
+          <div class="flex justify-between items-center">
+            <label for="exercise-type">Exercise type</label>
+            
+            
+            <select class="rounded-full p-3 bg-slate-700 text-gray-200 w-[240px]" name="exercise-type" id="exercise-type">
+
+                <option value="walk" selected>walk</option>
+                <option value="run">run</option>
+                <option value="cycle">cycle</option>
+
+            </select>
+        </div>
+
+
+          <div class="flex justify-between">
           
-      
+          <label for="exercise-distance">Distance</label>
+          <input type="text" name="exercise-distance" class="bg-slate-700 text-gray-200" />
+          
+        </div>
+
+        <div class="flex justify-between">
+
+          <label for="exercise-duration">Duration</label>
+          <input type="text" name="exercise-duration" class="bg-slate-700 text-gray-200" />
+
+        </div>
+
+        <div class="flex justify-end">
+            <button type="submit" class="bg-green-500 text-white p-4">SUBMIT</button>
+        </div>
+
+        
+
+          
+
+
           
 
       </div>
