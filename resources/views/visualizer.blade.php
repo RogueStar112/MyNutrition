17b6a88cd2c43cf20d489b9a9b249284d0996568
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-03-17 06:12:38 +0000

    Visualizer Demo (WIP)

--- resources/views/visualizer.blade.php
+++ resources/views/visualizer.blade.php
@@ -11,7 +11,7 @@
     @endphp
 
     <div class="py-4 relative mx-auto max-w-7xl">
-        <div id="TASK-VISUALIZER" class="max-w-7xl mx-auto /sm:px-6 /lg:px-8 even:bg-slate-800 flex [&>*]:grow h-[50vh] max-h-[300px] relative">
+        <div id="TASK-VISUALIZER" class="overflow-x-scroll max-w-7xl mx-auto /sm:px-6 /lg:px-8 even:bg-slate-800 flex [&>*]:grow h-[50vh] max-h-[300px] relative">
 
             <div id="TASKS" class=" w-full h-full absolute left-0 flex justify-center items-center /w-1/3 /h-1/5 border-4 border-orange-500 rounded-lg z-50 overflow-hidden">
                 
@@ -39,7 +39,16 @@
 
         </div>
 
+        <div class="flex justify-between items-center">
+        <div class="w-fit rounded-lg my-4 bg-slate-900 text-white p-4 flex items-center gap-6">Legend
+
+            <div class="bg-orange-500 h-[12px] w-[12px] rounded-full"></div>Meal
+            <div class="bg-yellow-500 h-[12px] w-[12px] rounded-full"></div>Exercise
+            <div class="bg-blue-500 h-[12px] w-[12px] rounded-full"></div>Sleep
+
+        </div>
         <div class="w-full text-right text-white sm:px-6 lg:px-8 mt-4">Calorie Target: 2500kcal</div>
+        </div>
     </div>
 
     <script>
@@ -55,6 +64,16 @@
 
         console.log(tv_width, tv_height);
 
+        const tv_cell_width = tv_width / 25;
+
+        function calibrateScreen() {
+            const tv_width = tv.width();
+            const tv_height = tv.height();
+            const tv_cell_width = tv_width / 25;
+        }
+
+        // window.onresize = calibrateScreen;
+ 
         function createNewTask(caption, color, x_start, x_end, y_index) {
 
             tasks.append(`<div class="bg-green-500 rounded-lg text-white absolute flex justify-center items-center h-8" style="background-color: #${color}; left: ${x_start}px; width:${x_end-x_start}px; top:${y_index}px;">${caption}</div>`);
@@ -63,8 +82,27 @@
 
         }
 
-        createNewTask('Exercise (30 min)', '33DD33', 210, 430, 0);
-        createNewTask('Breakfast (650kcal)', 'FF0000', 320, 640, 40);
-        createNewTask('Sleep', '2222FF', 800, 1400, 80);
+        function createNewTask_Time(caption, color, time_start, time_end, y_index) {
+
+           let x_time_start = time_start*tv_cell_width;
+           let x_time_end = time_end*tv_cell_width;
+
+            tasks.append(`<div class="bg-green-500 rounded-lg text-white absolute flex justify-center items-center h-8" style="background-color: #${color}; left: ${x_time_start}px; width:${(x_time_end-x_time_start)/tv_width*100}%; top:${y_index}px;">${caption}</div>`);
+
+        }
+
+        createNewTask_Time('Sleep', '0000FF', 0, 5.90, 0)
+        createNewTask_Time('Breakfast (600kcal)', 'FF0000', 6, 10.25, 10);
+        createNewTask_Time('Lunch (850kcal)', 'FF0000', 12, 17.75, 50)
+        createNewTask_Time('Snack', 'FF0000', 18, 19, 90)
+        createNewTask_Time('Dinner (1000kcal)', 'FF0000', 19, 25, 130);
+        createNewTask_Time('Sleep', '0000FF', 22, 25, 170)
+    
+        createNewTask_Time('Walk', 'BBBB33', 10.35, 11.90, 30)
+        // createNewTask('Walk (30 min)', '33BB33', 210, 430, 10);
+        // createNewTask('Breakfast (650kcal)', 'FF0000', 320, 640, 50);
+        // createNewTask('Sleep', '2222FF', 800, 1400, 90);
+
+        
     </script>
 </x-app-layout>
