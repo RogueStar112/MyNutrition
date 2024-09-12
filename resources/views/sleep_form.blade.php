5df200b031b178348ffd0d53e0bb657991d89b30
DMistica <hildtgunnt@gmail.com>
2024-09-12 21:32:34 +0100

    12092024 db + sleepform

--- resources/views/sleep_form.blade.php
+++ resources/views/sleep_form.blade.php
@@ -75,8 +75,319 @@
             
 
         </div>
+
+        <form id="WATER-FORM" method="POST" class="shadow-2xl max-w-[1216px] mx-auto relative" action="{{ route('water.store') }}">
+            @csrf
+    
+            
+            <i class="collapse md:visible fas fa-clock fa-3x p-4 text-blue-200 scale-[1.7] absolute right-[80px] bottom-16 translate-x-1/2 -translate-y-1/2"></i>
+            
+            {{-- background: radial-gradient(closest-side, #0465A5, #334155); --}}
+            <div style="background: radial-gradient(closest-side, #694F8E, #334155);">
+    
+            
+                <div class="w-full max-w-[1216px] /bg-[#0465A5] mx-auto text-center pt-4 text-white text-3xl">
+                    <p class="w-fit primary-sleep-form mx-auto p-4 -skew-x-12">Step one: Hours of sleep</p>
+                    <p id="assume-text" class="w-fit secondary-sleep-form mx-auto p-4 -skew-x-12 text-lg">Assume each bed is 1 hour.</p>
+                </div>
+        
+                <div class="w-full max-h-[256px] md:h-[128px] max-w-[1216px] mx-auto flex items-center justify-between px-2 md:px-24 py-12 md:py-0" style="/background-color: #0465A5; /background: radial-gradient(#0465A5, #9198e5);">
+        
+        
+                            <i class="fa-solid fa-circle-minus fa-3x text-white" id="decrease-glass"></i>
+        
+                            <div id="no-of-glasses" value="5" class="text-[#0465A5] bg-white rounded-full fa-2x p-4 duration-100 ">
+                                <i class="fa-solid fa-bed"></i>
+                                <i class="fa-solid fa-bed"></i>
+                                <i class="fa-solid fa-bed"></i>
+                                <i class="fa-solid fa-bed"></i>
+                                <i class="fa-solid fa-bed"></i>
+                            </div>
+        
+                            
+                            <i class="fa-solid fa-circle-plus fa-3x  text-white" id="increase-glass"></i>
+        
+                </div>
+                
+                                <p id="total-drank" class="text-white text-center /bg-[#0465A5] max-w-[1216px] mx-auto">You have slept for <span id="litres-drank">1 </span>hours.</p>
+                                <p class="text-white text-center /bg-[#0465A5] max-w-[1216px] mx-auto">That's <span id="glasses-drank">5</span> <span id="glasses-type">glasses</span> of <span class="drink-type">water</span>.</p>
+        
+                <div class="w-full max-w-[1216px] mx-auto /bg-[#0465A5] text-center pt-4 text-white text-3xl">
+                    <p class="w-fit primary-sleep-form mx-auto p-4 -skew-x-12">Step two: When slept</p>
+                    <p class="w-fit secondary-sleep-form mx-auto p-4 -skew-x-12 text-lg">When did you take this amount of <span class="drink-type">water</span>?</p>
+                </div>
+        
+        
+                <div class="w-full h-[128px] max-w-[1216px] mx-auto flex items-center justify-between px-24" style="/background-color: #0465A5; /background: radial-gradient(#e66465, #9198e5);">
+        
+                            <input type="hidden" name="water-amount" id="water-amount" value="5">
+                            <input type="hidden" name="fluid-type" id="fluid-type" value="0">
+                            <input id="water-when" name="water-when" class="mx-auto p-4 rounded-full text-[#0465A5]" type="datetime-local" value="2024-03-31T06:00">
+        
+                </div>
+        
+        
+                <div class="w-full h-[128px] max-w-[1216px] mx-auto flex items-center justify-between px-24 /bg-[#0465A5]">
+        
+                    <button type="submit" id="add-fluid-entry" class="bg-green-500 text-white mx-auto p-4">ADD ENTRY</button>
+        
+                </div>
+        
+                </div>
+        {{-- <div class="max-w-7xl mx-auto border-4 border-blue-500 p-24 dark:text-white relative my-6 mx-4 sm:px-6 lg:px-8">
+            <div class="py-4 relative mx-auto max-w-3xl overflow-hidden">
+                
+                <p>For context, A glass of water is about 200ml.</p>
+    
+            </div>
+    
+    
+            <form action="" class="text-white max-w-3xl flex gap-4 justify-between">
+    
+                <label for="water-time">
+                    Time drinking water:
+    
+                    <input type="datetime-local" id="water-time" name="water-time" class="block bg-slate-700 text-gray-200 w-full mt-1 rounded-md" placeholder="" value="" required/>
+                
+                </label>
+    
+    
+                <label for="no-of-glasses">
+                    Number of glasses (max 12)
+    
+                    <input type="number" id="no-of-glasses" name="no-of-glasses" class="block bg-slate-700 text-gray-200 w-full mt-1 rounded-md" min="0" max="12" placeholder="" value="" required/>
+                
+                </label>
+                
+                
+                
+            </form>
+    
+            <div class="flex justify-end">
+    
+                <button id="log-water-btn" class="bg-green-500 text-white px-6 mt-4 p-4">SUBMIT</button>
+    
+    
+    
+            </div>
+    
+            <form class="mt-4 flex flex-col gap-3"  id="WATER-LOG">
+    
+                <p class="text-3xl uppercase italic border-b-2 border-white">Water Log</p>
+    
+                
+    
+                
+                
+                
+                
+    
+            </form>
+    {{-- 
+            <i class="fas fa-tint fa-3x p-4 text-blue-400 opacity-60 scale-[3] -rotate-[30deg] absolute right-20 bottom-0 translate-x-1/2 -translate-y-1/2"></i>
+             --}}
+    </form> --}}
     </div>
 
-    
+
+    <script>
+
+        let nounToUse = 'water'
+
+        let drinkVal = $('#drink-type').val();
+        let verbToUse = drinkVal == 3 ? 'pieces' : 'glasses'
+
+        console.log('VTU', verbToUse)
+
+
+        const currentDate = new Date();
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
+
+
+        // make day or month have a leading zero if value is less than 10 e.g. 9 becomes 09.
+        if (day < 10) {
+
+            day = `0${day}`;
+        }
+
+        if (month < 10) {
+
+            month  = `0${month}`;
+
+        }
+
+        let hours = currentDate.getHours();
+        let minutes = currentDate.getMinutes();
+
+        // apply same leading zero logic to hours and minutes
+        if (hours < 10) {
+
+            hours = `0${hours}`;
+        }
+
+        if (minutes < 10) {
+
+            minutes = `0${minutes}`;
+
+        }
+
+        $('#water-when').val(`${year}-${month}-${day}T${hours}:${minutes}`);
+
+        $('#water-when').attr('value', `${year}-${month}-${day}T${hours}:${minutes}`);
+
+        let no_of_hrs_slept = 5;
+
+        $('#increase-glass, #decrease-glass').on('click', function() {
+
+            if(this.id == 'increase-glass') {
+                no_of_hrs_slept += 1;
+                } else {
+                no_of_hrs_slept -= 1;
+            }
+
+            if(no_of_hrs_slept > 0) {
+
+
+            } else {
+                
+                no_of_hrs_slept = 0;
+
+            }
+
+
+            let output_str = '';
+            for(let i = 0; i<no_of_hrs_slept; i++) {
+                if($('#drink-type').find(":selected").val() == 3) {
+                    output_str += '<i class="fas fa-apple-alt"></i> ';
+                } else {
+                    output_str += '<i class="fa-solid fa-bed"></i> ';
+                }
+
+            }
+
+
+            $('#no-of-glasses').html(output_str);
+            // $('#litres-drank').html(Number.parseFloat(1*no_of_hrs_slept).toFixed(2));
+            $('#litres-drank').html(` ${1*no_of_hrs_slept}`);
+            $('#glasses-drank').html(no_of_hrs_slept)
+            $('#no-of-glasses').attr('value', no_of_hrs_slept);
+            $('#water-amount').val(no_of_hrs_slept)
+        })
+
+        // $('#log-water-btn').on('click', function() {
+
+        //     let water_time = $('#water-time').val();
+        //     let water_glasses = $('#no-of-glasses').val();
+
+        //     console.log(water_time, water_glasses)
+
+        //     $('#WATER-LOG').append('<p>BABA BOOEY</p>');
+
+
+        // });
+
+        $('#drink-type').on('change', function() {
+
+            // console.log(this)
+            // console.log(this.value);
+            
+            //IF WATER
+            if(this.value == 0) {
+
+                $(".primary-sleep-form").css("background-color", "#14b8a6");
+                  $(".primary-sleep-form").css("color", "rgb(255, 255, 255)");
+
+                $(".secondary-sleep-form").css("background-color", "#0d9488");
+                $(".tertiary-water-form").css("background-color", "#0f766e");
+
+                $("#no-of-glasses>*").addClass('fa-solid fa-bed');
+                $("#no-of-glasses>*").removeClass('fas fa-apple-alt');
+                
+                $('#glasses-type').text('glasses')
+
+                $('#assume-text').text('Assume each glass is 200ml.')
+
+                $('#total-drank').removeClass('hidden');
+
+                $('#fluid-type').val(this.value);
+            }
+            
+            // IF COKE
+            if(this.value == 1) {
+
+                $(".primary-sleep-form").css("background-color", "rgb(154, 81, 26)");
+                $(".primary-sleep-form").css("color", "rgb(255, 255, 255)");
+                $(".secondary-sleep-form").css("background-color", "rgb(115, 60, 19)");
+                $(".tertiary-water-form").css("background-color", "rgb(95, 60, 19)");
+
+               $("#no-of-glasses>*").addClass('fa-solid fa-bed');
+               $("#no-of-glasses>*").removeClass('fas fa-apple-alt');
+
+                $('#glasses-type').text('glasses')
+
+                $('#assume-text').text('Assume each glass is 200ml.')
+
+                $('#total-drank').removeClass('hidden');
+
+                $('#fluid-type').val(this.value);
+            }
+
+
+            // IF MILK
+            if(this.value == 2) {
+
+                $(".primary-sleep-form").css("background-color", "rgb(254,250,235)");
+                $(".primary-sleep-form").css("color", "rgb(0, 0, 0)");
+                $(".secondary-sleep-form").css("background-color", "rgb(135, 135, 135)");
+                $(".tertiary-water-form").css("background-color", "rgb(125, 125, 125)");
+
+                $("#no-of-glasses>*").addClass('fa-solid fa-bed');
+                $("#no-of-glasses>*").removeClass('fas fa-apple-alt');
+                
+                $('#glasses-type').text('glasses')
+
+                $('#assume-text').text('Assume each glass is 200ml.')
+
+                $('#total-drank').removeClass('hidden');
+
+                $('#fluid-type').val(this.value);
+            }
+
+            // IF FRUIT
+            if(this.value == 3) {
+
+            
+                $(".primary-sleep-form").css("background-color", "rgb(0, 80, 0)");
+                $(".primary-sleep-form").css("color", "rgb(0, 230, 0)");
+                $("#no-of-glasses>*").removeClass('fa-solid fa-bed');
+                $("#no-of-glasses>*").addClass('fas fa-apple-alt');
+
+
+
+                $(".secondary-sleep-form").css("background-color", "rgb(0, 155, 0)");
+                $(".tertiary-water-form").css("background-color", "rgb(0, 135, 0)");
+                
+                $('#glasses-type').text('pieces');
+
+                $('#assume-text').text('Assume each fruit is part of your 5 a day.')
+
+                $('#total-drank').addClass('hidden');
+
+                $('#fluid-type').val(this.value);
+
+            }
+
+            $('.drink-type').text($(this).find(":selected").attr('text-value'))
+
+
+
+        });
+
+
+        
+
+    </script>   
 
 </x-app-layout>
