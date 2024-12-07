45f2229d70938f04c00c65a889d90afe67519bb2
DMistica <hildtgunnt@gmail.com>
2024-12-07 06:03:21 +0000

    Sort migrations 6

--- database/migrations/2024_08_29_160103_make_water_table.php
+++ database/migrations/2024_08_29_160103_make_water_table.php
@@ -18,18 +18,22 @@ return new class extends Migration
         //     $table->string('name');
         // });
 
-        Schema::create('water', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('fluid_id');
-            $table->float('amount', 8, 1);
-            $table->timestamp('time_taken');
-            $table->timestamps();
+        // Schema::create('water', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->unsignedBigInteger('user_id');
+        //     $table->unsignedBigInteger('fluid_id');
+        //     $table->float('amount', 8, 1);
+        //     $table->timestamp('time_taken');
+        //     $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('fluid_id')->references('id')->on('fluid_type');
+        //     $table->foreign('user_id')->references('id')->on('users');
+        //     $table->foreign('fluid_id')->references('id')->on('fluid_type');
 
-        });
+        // });
+
+        // Schema::table('fluid_type', function (Blueprint $table) {
+        //     $table->string('name');
+        // })
 
 
         // DB::table('fluid_type')->insert([
