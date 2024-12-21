09375167b8d09e590eca26298dfe02ad8189c5b2
DMistica <hildtgunnt@gmail.com>
2024-12-21 21:09:26 +0000

    mealcontroller fix

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -660,29 +660,31 @@ class MealController extends Controller
 
         $check_if_local = env('DB_CONNECTION') === 'sqlite';
         
-        // if ($check_if_local) {
+        if ($check_if_local) {
             $meal_dates_select_filterstr = "strftime('%Y-%m-%d', time_planned) as date, time_planned";
             $exercise_dates_select_filterstr = "strftime('%Y-%m-%d', exercise_start) as date, exercise_start";
 
-            // $meal_dates_select =        DB::table('meal')
-            //                                 ->selectRaw($meal_dates_select_filterstr)
-            //                                 ->where('user_id', 1)
-            //                                 ->where('is_eaten', 1)
-            //                                 ->groupBy('time_planned')
-            //                                 ->orderBy('time_planned', 'desc')
-            //                                 ->distinct();
-            //                                 ->get();
-        // } else {
-            // $meal_dates_select_filterstr = "DISTINCT DATE_FORMAT(time_planned, '%Y-%m-%d') as date, time_planned";
-            // $exercise_dates_select_filterstr = "DISTINCT DATE_FORMAT(exercise_start, '%Y-%m-%d') as date, time_planned";
+            $meal_dates_select =        DB::table('meal')
+                                            ->selectRaw($meal_dates_select_filterstr)
+                                            ->where('user_id', 1)
+                                            ->where('is_eaten', 1)
+                                            ->groupBy('time_planned')
+                                            ->orderBy('time_planned', 'desc')
+                                            ->distinct()
+                                            ->get();
+
+
+        } else {
+            $meal_dates_select_filterstr = "DISTINCT DATE_FORMAT(time_planned, '%Y-%m-%d') as date, time_planned";
+            $exercise_dates_select_filterstr = "DISTINCT DATE_FORMAT(exercise_start, '%Y-%m-%d') as date, exercise_start";
             
-            // $meal_dates_select =        DB::table('meal')
-            //                                 ->selectRaw($meal_dates_select_filterstr)
-            //                                 ->where('user_id', 1)
-            //                                 ->where('is_eaten', 1)
-            //                                 ->orderBy('time_planned', 'desc')
-            //                                 ->get();
-        // }
+            $meal_dates_select =        DB::table('meal')
+                                            ->selectRaw($meal_dates_select_filterstr)
+                                            ->where('user_id', 1)
+                                            ->where('is_eaten', 1)
+                                            ->orderBy('time_planned', 'desc')
+                                            ->get();
+        }
 
 
 
